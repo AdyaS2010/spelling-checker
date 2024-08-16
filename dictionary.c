55ab97fb976491ca8c0dfa03f0c190414d05f232
AdyaS2010 <adya.sastry@gmail.com>
2024-08-15 21:59:47 -0500

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -67,3 +67,77 @@ unsigned int hash(const char *word)
     }
     return hash % N;
 }
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // TODO
+    // Open dictionary file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+    // Insert words into the hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // Allocate memory for new node
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            unload();
+            return false;
+        }
+        // Copy word into node
+        strcpy(n->word, word);
+        // Hash word to get hash value
+        int index = hash(word);
+        // Insert node into hash table
+        n->next = table[index];
+        table[index] = n;
+
+        // Increment word count
+        word_count++;
+    }
+
+    // Close dictionary
+    fclose(file);
+
+    // Indicate success
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return word_count;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    // Iterate over each element in hash table
+    for (int i = 0; i < N; i++)
+    {
+        // Access linked list at specified index in hash table
+        node *cursor = table[i];
+        // Go through linked list & free the nodes
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+        // Check if memory was freed successfully
+        if (table[i] != NULL)
+        {
+            return false;
+        }
+    }
+    return true;
+}
